4343cdf3c910615b8f7320f3b184521a1c09e7ac
NotRoyadma <82922092+NotRoyadma@users.noreply.github.com>
2022-03-27 08:48:48 +0530

    Update fixcpuram.sh

--- everything_in_one/fixcpuram.sh
+++ everything_in_one/fixcpuram.sh
@@ -25,32 +25,35 @@ mkdir /fixramcpu
 cd /fixramcpu
 
 /bin/sleep 1
-
+echo ""
 echo "Downlaoding The Docker Files"
 curl -O https://download.docker.com/linux/static/stable/x86_64/docker-20.10.9.tgz
 
 /bin/sleep 1
-
+echo ""
 echo "Extracting Files"
 tar -xzvf docker-20.10.9.tgz
 
 /bin/sleep 1
-
+echo ""
 echo "Moving Files to Main Directory"
-cd /fixramcpu
+cd /fixramcpu/docker/
 mv * /usr/bin/ 
 
 
 /bin/sleep 1
-
+echo ""
 echo "Finalizing The Steps!"
+rm -rf /fixramcpu
 
 /bin/sleep 1
 echo ""
-echo "####################################"
-echo "# The Fixup Was Successfull!"
-echo "# Thanks for Using Our Script"
-echo "# If You Find Any Issues Regarding This, Please Report at - https://dnxrg.net/discord"
-echo "# "
-echo "# Run reboot Command To Make it Work!"
+echo "##########################################################################################"
+echo "#                                                                                        #"
+echo "# The Fixup Was Successfull!                                                             #"
+echo "# Thanks for Using Our Script                                                            #"
+echo "# If You Find Any Issues Regarding This, Please Report at - https://dnxrg.net/discord    #"
+echo "#                                                                                        #"
+echo "# Run reboot Command To Make it Work!                                                    #"
+echo "#                                                                                        #"
 echo "####################################"
