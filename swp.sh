246c87a568347582f9723618ed00572a17c99b8d
NotRoyadma <82922092+NotRoyadma@users.noreply.github.com>
2022-03-27 07:38:45 +0530

    Update swp.sh

--- swp.sh
+++ swp.sh
@@ -15,19 +15,42 @@ echo "Mention The Amout Of Swap You Wanna Install in GB (ex. 200)"
 
 read swp_space
 
+/bin/sleep 1
+
+
 swapon --all
+/bin/sleep 1
 
-fallocate -l "$swp_space"g/mnt/$swp_spaceGiB.swap
+fallocate -l "$swp_space""g" /mnt/"$swp_space""GiB.swap"
 
-chmod 600 /mnt/"$swp_space"GiB.swap
+/bin/sleep 1
 
-mkswap /mnt/"$swp_space"GiB.swap
+chmod 600 /mnt/"$swp_space""GiB.swap"
 
-swapon /mnt/"$swp_space"GiB.swap
+/bin/sleep 1
+
+mkswap /mnt/"$swp_space""GiB.swap"
+
+/bin/sleep 1
+
+swapon /mnt/"$swp_space""GiB.swap"
+
+/bin/sleep 1
 
 cat /proc/swaps
 
-echo '/mnt/"$swp_space"GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
+/bin/sleep 1
+
+free -h
+
+#echo '/mnt/"$swp_space"GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
+echo ""
+echo "Successfully Installed $swp_space GB of SWAP"
+echo ""
+echo "Rebooting The System"
+
+/bin/sleep 1
+
 
 reboot
 
